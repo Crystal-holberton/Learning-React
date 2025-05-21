112d6ddbd6e1c9e65ae5cef2bf39c8d29abddd92
Crystal-holberton <9188@holbertonstudents.com>
2025-05-21 14:20:28 +1000

    adding a card

--- My-react-app/src/App.jsx
+++ My-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Card from './Card.jsx';
 
 function App() {
 
   return(
     <>
       <Header/>
+      <Card/>
       <Footer/>
     </>
   );
---
+++ My-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Crystal</h2>
+            <p>I am studying at Holberton and play video games</p>
+        </div>
+    );
+}
+
+export default Card
